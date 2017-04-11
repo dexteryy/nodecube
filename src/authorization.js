1c50299e7fb2f764dd12ad15d9092aef51245ea3
Dexter Yang <dexter.yy@gmail.com>
2017-04-11 21:15:38 +0800

    feat: support token in query

--- src/authorization.js
+++ src/authorization.js
@@ -3,14 +3,37 @@ import jwtMiddleware from 'express-jwt';
 
 export default function authorization({
   jwtSecret,
+  credentialsRequired = true,
+  queryTokenName = 'token',
+  getToken,
   publicPath = [],
   unauthorizedMessage = 'INVALID TOKEN',
 }) {
+
+  function fromHeaderOrQuery(req) {
+    const {
+      headers: {
+        authorization,
+      },
+      query: {
+        [queryTokenName]: token,
+      },
+    } = req;
+    const authHeader = authorization ? authorization.split(' ') : [];
+    if (authHeader[0] === 'Bearer') {
+      return authHeader[1];
+    } else if (token) {
+      return token;
+    }
+    return null;
+  }
 
   return {
 
     authorized: jwtMiddleware({
       secret: jwtSecret,
+      credentialsRequired,
+      getToken: getToken || fromHeaderOrQuery,
     }).unless({
       path: publicPath,
     }),
